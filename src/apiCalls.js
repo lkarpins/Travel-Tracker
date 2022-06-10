5f1c3a63c202a019528b67140b467286ee91182e
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-06-10 11:29:29 -0400

    Added traveler-test file, html changes for trip selection, and formatting

---
+++ src/apiCalls.js
@@ -0,0 +1,19 @@
+// const fetchApiData = type => {
+//   return fetch(`http://localhost:3001/api/v1/${type}`)
+//     .then(response => response.json())
+//     .then(data => data)
+//     .catch(error => console.log(`${type} API error!`));
+// };
+//
+// const fetchData = () => {
+//   return Promise.all([
+//     fetchApiData(),
+//     fetchApiData(),
+//     fetchApiData(),
+//     fetchApiData()
+//   ]);
+// };
+//
+// export default {
+//   fetchData
+// };
