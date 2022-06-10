88fba3846dc54a3f75a0191079d89b0d48e6d911
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-06-09 20:43:37 -0400

    Added files for traveler, trip, destionation classes

---
+++ src/traveler.js
@@ -0,0 +1,14 @@
+class Traveler {
+  constructor() {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.travelerType = userData.travelerType;
+  }
+
+  returnFirstName = () => {
+    const firstName = this.name.split(" ");
+    return firstName[0];
+  };
+}
+
+export default User;
